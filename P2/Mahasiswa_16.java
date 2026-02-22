82729b84e4ad0e76323f33f2a0c6d1616bfc221a
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-02-22 12:39:16 +0700

    Pertanyaan 4 Prak 1 (revisi) dan Prak 3

--- P2/Mahasiswa_16.java
+++ P2/Mahasiswa_16.java
@@ -1,11 +1,22 @@
 package P2;
 public class Mahasiswa_16 {
     
-        String nama16, nim16, kelas16;
+        String nama16, nim16, kelas16,nm16;
         double ipk16;
+
+        public Mahasiswa_16() {
+        
+    }
+
+    public Mahasiswa_16(String nm16, String nim16, double ipk16,  String kls16) {
+        nama16 = nm16;
+        this.nim16 = nim16;
+        this.ipk16 = ipk16;
+        kelas16 = kls16;
+    }
         
     void tampilkanInformasi(){
-        System.out.println("nama: "+nama16);
+        System.out.println("Nama: "+nama16);
         System.out.println("Nim: "+nim16);
         System.out.println("IPK: "+ipk16);
         System.out.println("Kelas: "+kelas16);
@@ -14,7 +25,7 @@ public class Mahasiswa_16 {
         kelas16=Kelasbaru;
     }
     void updateIPK(double ipkbaru){
-    if (ipk16<=0.0||ipk16>=4.0) {
+    if (ipk16>=0.0 && ipk16<=4.0) {
         
         ipk16=ipkbaru;
     }else
