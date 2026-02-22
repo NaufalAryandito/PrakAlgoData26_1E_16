ef2c9ac3587e02da844ff2934c3f973c05372363
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-02-22 12:39:59 +0700

    Prak 3 Jobsheet 2

--- P2/MahasiswaMain_16.java
+++ P2/MahasiswaMain_16.java
@@ -2,21 +2,22 @@ package P2;
 
 public class MahasiswaMain_16 {
     public static void main(String[] args) {
-        Mahasiswa_16 mhs16= new Mahasiswa_16();
-        mhs16.nama16="Dito";
-        mhs16.kelas16="TI-1E";
-        mhs16.nim16="254107020108";
-        mhs16.ipk16=3.6; 
-        System.out.println("Data Mahasiswa Awal");
+        Mahasiswa_16 mhs16 = new Mahasiswa_16();
+        mhs16.nama16 = "Muhammad Naufal Aryandito A.";
+        mhs16.kelas16 = "TI-1E";
+        mhs16.nim16 = "254107020108";
+        mhs16.ipk16 = 3.6;
+       
         mhs16.tampilkanInformasi();
-    mhs16.updateKelas("TI-2E");
-    mhs16.updateIPK(3.9);
-    System.out.println("Data Mahasiswa Update");
-    mhs16.tampilkanInformasi();
+        mhs16.updateKelas("TI-2E");
+        mhs16.updateIPK(3.9);
+        mhs16.tampilkanInformasi();
 
+        Mahasiswa_16 mhs2= new Mahasiswa_16("Annisa Nabila", "2141720160", 3.25, "TI-2L" );
+            mhs2.updateIPK(3.30);
+            mhs2.tampilkanInformasi();
+        
 
-
-}
-    
+    }
 
 }
