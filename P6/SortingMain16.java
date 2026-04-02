7270b8ea964b9c827c8e7aac14028579f39febbd
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-04-02 09:52:25 +0700

    Praktikum 1 JS 6

---
+++ P6/SortingMain16.java
@@ -0,0 +1,22 @@
+package P6;
+
+public class SortingMain16 {
+    public static void main(String[] args) {
+        int a[]= {20, 10, 2, 7, 12};
+        int b[]= {30, 20, 2, 8, 14};
+        Sorting16 dataurut1 = new Sorting16(a, a.length);
+        Sorting16 dataurut2 = new Sorting16 (b, b.length);
+        System.out.println("Data awal 1");
+        
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+    //     System.out.println("Data awal 2");
+    //     dataurut2.tampil();
+    //     dataurut2.SelectionSort();
+    //     System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+    //     dataurut2.tampil();
+    }
+}
