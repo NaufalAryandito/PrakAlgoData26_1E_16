239086bdf88cc55f1fecae3506d39a0a1c85047d
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-04-27 20:34:10 +0700

    Praktikum 1 Jobsheet 9

---
+++ P9/Mahasiswa16.java
@@ -0,0 +1,16 @@
+package P9;
+public class Mahasiswa16 {
+    String nim, nama, kelas;
+    int nilai;
+    
+    Mahasiswa16(String nama, String nim, String kelas){
+        this.nama=nama;
+        this.nim=nim;
+        this.kelas=kelas;
+        nilai= -1;
+    }
+    void tugasDinilai(int nilai){
+        this.nilai=nilai;
+    }
+
+}
