6f6292fc4b3b830af7e889919c543ffb121ba2f0
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-04-27 22:42:12 +0700

    Tugas Latihan Jobsheet 9

---
+++ P9/Surat16.java
@@ -0,0 +1,17 @@
+package P9;
+
+public class Surat16 {
+    String idSurat;
+    String namaMahasiswa;
+String kelas;
+char jenisIzin;
+int durasi;
+
+Surat16(String id, String nama, String kelas, char jenis, int durasi){
+    idSurat=id;
+    namaMahasiswa=nama;
+    this.kelas=kelas;
+    jenisIzin= jenis;
+    this.durasi=durasi;
+}
+}
