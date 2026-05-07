9ae53fb38ae800a5e6c83d2c2f149fdc1654df63
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-05-07 09:03:00 +0700

    Pertanyaan 3 Praktikum 1 Jobsheet 11

--- P11/SLLMain16.java
+++ P11/SLLMain16.java
@@ -1,5 +1,7 @@
 package P11;
+
 import java.util.Scanner;
+
 public class SLLMain16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,7 +10,19 @@ public class SLLMain16 {
         Mahasiswa16 mhs2 = new Mahasiswa16("22212202", "Cintia", "3C", 3.5);
         Mahasiswa16 mhs3 = new Mahasiswa16("22212202", "Bimon", "2B", 3.8);
         Mahasiswa16 mhs4 = new Mahasiswa16("21212203", "Dirga", "4D", 3.6);
-        
+
+        System.out.print("Masukkan nama  : ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan NIM   : ");
+        String nim = sc.nextLine();
+        System.out.print("Masukkan kelas : ");
+        String kelas = sc.nextLine();
+        System.out.print("Masukkan IPK   : ");
+        double ipk = sc.nextDouble();
+
+        Mahasiswa16 baru = new Mahasiswa16(nim, nama, kelas, ipk);
+        sll.addLast(baru);
+        sll.print();
         sll.print();
         sll.addFirst(mhs4);
         sll.print();
