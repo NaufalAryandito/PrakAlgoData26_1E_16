1d292f0345633b2531e38c5383ae1b1845e8d314
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-06-17 22:02:35 +0700

    Pertanyaan 2 Praktikum 1 Jobsheet 16

--- P16/ContohList16.java
+++ P16/ContohList16.java
@@ -6,12 +6,10 @@ import java.util.List;
 
 public class ContohList16 {
     public static void main(String[] args) {
-        List l = new ArrayList();
-
+        List<Integer> l = new ArrayList<>();
         l.add(1);
         l.add(2);
         l.add(3);
-        l.add("Cireng");
 
         System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
