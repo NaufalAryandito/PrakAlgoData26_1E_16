416c99ec04b02e718f2977335a89b90eb08f729a
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-06-17 23:33:24 +0700

    Pertasnyaan 2 dan 3 Praktikum 3 Jobsheet 16

--- P16/ListMahasiswa16.java
+++ P16/ListMahasiswa16.java
@@ -1,53 +1,69 @@
 package P16;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ListMahasiswa16 {
-    List<Mahasiswa16> mahasiswas = new ArrayList<>(); 
+    List<Mahasiswa16> mahasiswas = new ArrayList<>();
 
-    
-    public void tambah(Mahasiswa16... mahasiswa) { 
-        mahasiswas.addAll(Arrays.asList(mahasiswa)); 
+    public void tambah(Mahasiswa16... mahasiswa) {
+        mahasiswas.addAll(Arrays.asList(mahasiswa));
     }
 
-    public void hapus(int index) { 
+    public void hapus(int index) {
         mahasiswas.remove(index);
     }
 
-    public void update(int index, Mahasiswa16 mhs) { 
-        mahasiswas.set(index, mhs); 
+    public void update(int index, Mahasiswa16 mhs) {
+        mahasiswas.set(index, mhs);
     }
 
     public void tampil() {
         mahasiswas.stream().forEach(mhs -> {
-            System.out.println("" + mhs.toString()); 
+            System.out.println("" + mhs.toString());
         });
     }
 
-  
-    int linearSearch(String nim) { 
-        for (int i = 0; i < mahasiswas.size(); i++) {
-            if (nim.equals(mahasiswas.get(i).nim)) { 
-                return i; 
-            }
-        }
-        return -1; 
+    int binarySearch(String nim) {
+
+        sortAscending();
+
+        Mahasiswa16 key = new Mahasiswa16(nim, "", "");
+
+        return Collections.binarySearch(mahasiswas, key, (m1, m2) -> m1.nim.compareTo(m2.nim));
     }
+
+    public void sortAscending() {
+        mahasiswas.sort((m1, m2) -> m1.nim.compareTo(m2.nim));
+    }
+
+    public void sortDescending() {
+        mahasiswas.sort((m1, m2) -> m2.nim.compareTo(m1.nim));
+    }
+
     public static void main(String[] args) {
-        ListMahasiswa16 lm = new ListMahasiswa16(); 
+        ListMahasiswa16 lm = new ListMahasiswa16();
 
-        Mahasiswa16 m = new Mahasiswa16("201234", "Noureen", "021xx1"); 
-        Mahasiswa16 m1 = new Mahasiswa16("201235", "Akhleema", "021xx2"); 
-        Mahasiswa16 m2 = new Mahasiswa16("201236", "Shannum", "021xx3"); 
+        Mahasiswa16 m = new Mahasiswa16("201234", "Noureen", "021xx1");
+        Mahasiswa16 m1 = new Mahasiswa16("201235", "Akhleema", "021xx2");
+        Mahasiswa16 m2 = new Mahasiswa16("201236", "Shannum", "021xx3");
 
-        lm.tambah(m, m1, m2); 
-        lm.tampil(); 
-        int indexCari = lm.linearSearch("201235"); 
-        lm.update(indexCari, new Mahasiswa16("201235", "Akhleema Lela", "021xx2")); 
+        lm.tambah(m, m1, m2);
+        lm.tampil();
+        int indexCari = lm.binarySearch("201235");
+
+        if (indexCari >= 0) {
+            lm.update(indexCari, new Mahasiswa16("201235", "Akhleema Lela", "021xx2"));
+        }
+        System.out.println("\n--- Diurutkan Secara Descending (NIM Terbesar) ---");
+        lm.sortDescending();
+        lm.tampil();
+        System.out.println("\n--- Setelah Data Di-update ---");
+        lm.tampil();
 
         System.out.println("");
-        lm.tampil(); 
+        lm.tampil();
     }
-} 
-
+}
